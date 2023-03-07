138780b0e942c8a6e25c1b3e8ecfdc0034693947
FilippoLeone <filippo.leone@outlook.com>
2023-03-08 00:27:22 +0100

    fix 2

--- Lib/Info.php
+++ Lib/Info.php
@@ -1,7 +1,7 @@
 <?php 
     $project_name = "";
     $company_name = "FitConncts"; 
-    $server_name = "http://fitconnects.altervista.org/"; //es. www.fitcon.com or localhost , cambiare se non in locale  
+    $server_name = "https://fit-connects.herokuapp.com/"; //es. www.fitcon.com or localhost , cambiare se non in locale  
 
     define("COMP_NAME",$company_name);
     define("SERV_NAME",$server_name);
